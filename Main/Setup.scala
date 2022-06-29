c5c3619a71690acf564d3444eb68fc36d619db4a
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-06-29 17:05:05 -0400

    Subtables can be made

--- Main/Setup.scala
+++ Main/Setup.scala
@@ -22,8 +22,8 @@ object Setup { // Object file name.
     println("Created spark session.")
     /*Anything above this has been mostly unchanged (generalized)*/
 
-    val df2 = spark.read.format("csv").option("sep",",").load("hdfs://localhost:9000/user/hive/warehouse/MortalityDatabase.csv") // File location in hdfs
-    df2.createOrReplaceTempView("mortalitydata")   //Temp table name given     //Type didn't seem to matter, just loaded straight .txt that looked like tables.
+    val df1 = spark.read.format("csv").option("sep",",").load("hdfs://localhost:9000/user/hive/warehouse/MortalityDatabase.csv") // File location in hdfs
+    df1.createOrReplaceTempView("mortalitydata")   //Temp table name given     //Type didn't seem to matter, just loaded straight .txt that looked like tables.
     spark.sql("SELECT * FROM mortalitydata;").show()                     // Show() displays current tables.
     spark.sql("SELECT * FROM mortalitydata WHERE _c4=2020;").show() //Shows all tables that have 2020
     spark.sql("SELECT * FROM mortalitydata WHERE _c7 LIKE '%55%';").show() // All rows that show 55 years in column c7_
@@ -33,5 +33,24 @@ object Setup { // Object file name.
     spark.sql("SELECT _c6, SUM(_c8) AS Msum FROM mortalitydata WHERE _c4=2020 GROUP BY _c6;").show() //Sums all deaths for 2020 and group by race.
                         //_c6 = Race, _c8 = Total Deaths, _c4 = Year
     //https://www.eversql.com/sql-syntax-check-validator/
+
+
+    var Mdf1 = spark.sql("SELECT * FROM mortalitydata WHERE _c6 LIKE '%Black%';")// Creates a sub-table to be viewed.
+    Mdf1.distinct().count()                                                                     // Can't have .show() if you want to save as var
+    Mdf1.createOrReplaceTempView(viewName="Mmortalitydata")
+    spark.sql("SELECT _c7, SUM(_c8) AS Msum FROM Mmortalitydata GROUP BY _c7 ORDER BY CAST(Msum as int);").show() //Shows sum of all black deaths
+                                                                                                            //Orders from least to most and groups by ages
+    //Print results with println() to see if you can use a specific row.
   }
 }
+
+
+
+
+
+
+
+
+
+
+
