dcbfb6b777059a25f12d8f10743fa27451c937c7
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-06-29 13:06:23 -0400

    Added more SQL functionality

--- Main/Setup.scala
+++ Main/Setup.scala
@@ -27,7 +27,11 @@ object Setup { // Object file name.
     spark.sql("SELECT * FROM mortalitydata;").show()                     // Show() displays current tables.
     spark.sql("SELECT * FROM mortalitydata WHERE _c4=2020;").show() //Shows all tables that have 2020
     spark.sql("SELECT * FROM mortalitydata WHERE _c7 LIKE '%55%';").show() // All rows that show 55 years in column c7_
-
+    spark.sql("SELECT * FROM mortalitydata WHERE _c6 LIKE '%Black%'  AND _c5=2;").show() //Shows all black values in month 2.
+    spark.sql("SELECT * FROM mortalitydata WHERE _c6 LIKE '%Black%'  AND _c5=2 ORDER BY _c10 + 0 DESC LIMIT 10;").show() //Same but limits to 10 and orders by total septicemia
+                                                           //Can also convert to integer with ORDER_BY cast(_c10 as int) ASC/DESC
+    spark.sql("SELECT _c6, SUM(_c8) AS Msum FROM mortalitydata WHERE _c4=2020 GROUP BY _c6;").show() //Sums all deaths for 2020 and group by race.
+                        //_c6 = Race, _c8 = Total Deaths, _c4 = Year
     //https://www.eversql.com/sql-syntax-check-validator/
   }
 }
