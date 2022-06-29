4ebecbfce82fdcfc166315e006ff17b06e7ea7ad
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-06-29 01:41:05 -0400

    Main scala file updated to work with new database

--- Main/Setup.scala
+++ Main/Setup.scala
@@ -1,10 +1,17 @@
+package Project1
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
-object Setup{ // Object file name.
+object Setup { // Object file name.
   def main(args: Array[String]): Unit = {             //Need hadoop, scala2.13, hive and jdk8 installed.
     // create a spark session
     // for Windows
+    //val data = request.get("https://data.cdc.gov/api/views/r5pw-bk5t/rows.json?accessType=DOWNLOAD")
+    //val text = data.text()
+    //val json = ujson.read(text)
+    //os.write(os.pwd/"tmp.json",json)
+
     val spark = SparkSession
       .builder
       .appName("hello hive")
@@ -15,10 +22,12 @@ object Setup{ // Object file name.
     println("Created spark session.")
     /*Anything above this has been mostly unchanged (generalized)*/
 
-    val df2 = spark.read.format("csv").option("sep",",").load("hdfs://localhost:9000/user/hive/warehouse/Bev_BranchA.txt") // File location
-    df2.createOrReplaceTempView("beverage")                       //Type didn't seem to matter, just loaded straight .txt that looked like tables.
-    spark.sql("SELECT * FROM beverage;").show()                     // Show() displays current tables.
-    spark.sql("SELECT _c0 from beverage WHERE _c1='Branch6'").show() //Shows all tables that have Branch 6
+    val df2 = spark.read.format("csv").option("sep",",").load("hdfs://localhost:9000/user/hive/warehouse/MortalityDatabase.csv") // File location in hdfs
+    df2.createOrReplaceTempView("mortalitydata")   //Temp table name given     //Type didn't seem to matter, just loaded straight .txt that looked like tables.
+    spark.sql("SELECT * FROM mortalitydata;").show()                     // Show() displays current tables.
+    spark.sql("SELECT * FROM mortalitydata WHERE _c4=2020;").show() //Shows all tables that have 2020
+    spark.sql("SELECT * FROM mortalitydata WHERE _c7 LIKE '%55%';").show() // All rows that show 55 years in column c7_
 
+    //https://www.eversql.com/sql-syntax-check-validator/
   }
 }
