fa2c9ecaf7dd266496c42dea6dee7a56aad8e8a0
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-07-11 04:00:12 -0400

    Menu Made

--- Main/mainUA.scala
+++ Main/mainUA.scala
@@ -5,37 +5,63 @@ object mainUA {
   def main(args: Array[String]): Unit = {
 
     functionsIMP.connectlink()
+    var running: String = "True"
 
-    println("What is your username?")
-    functionsIMP.passwordmanagement("admin") //Input username and check for password. scala.io.StdIn.readLine()
+    while(running != "End") {
+      println("What do you wish to do?")
+      running = scala.io.StdIn.readLine()
+      println("Status----------------->" + running + " Selected, Loading Process")
 
-    println("What column do you wish to see?")
-    functionsIMP.all2021column("All_Deaths") // First one has to ask prompt after connecting.   scala.io.StdIn.readLine()
+      if (running=="1"){
+        println("What is your username?")
+        functionsIMP.passwordmanagement("admin") //Input username and check for password. scala.io.StdIn.readLine()
+      }
 
-    functionsIMP.connectlink()
+      if (running=="2") {
+        println("What column do you wish to see?")
+        functionsIMP.all2021column("All_Deaths") // First one has to ask prompt after connecting.   scala.io.StdIn.readLine()
+      }
 
-    functionsIMP.agegroup()
+      if (running=="3") {
+        functionsIMP.connectlink()
+      }
 
-    functionsIMP.yeargroup()
+      if (running=="4") {
+        functionsIMP.agegroup()
+      }
 
-    functionsIMP.monthgroup()
+      if (running=="5") {
+        functionsIMP.yeargroup()
+      }
 
-    //-------------------------------------------------------------------------------------------------------------
-    functionsIMP.diseasegroup("Heart_Disease") //Least amount of people go to the hospital during holidays.
+      if (running=="6") {
+        functionsIMP.monthgroup()
+      }
 
-    functionsIMP.diseasegroup("Chronic_Respiratory_Diseases")
-    //-------------------------------------------------------------------------------------------------------------
+      if (running=="7") {
 
-    //functionsIMP.calcdiff()
+        functionsIMP.diseasegroup("Heart_Disease") //Least amount of people go to the hospital during holidays.
+        functionsIMP.diseasegroup("Chronic_Respiratory_Diseases")
+      }
 
-    functionsIMP.mostvulnerable()
+      //functionsIMP.calcdiff()
+      if (running=="8") {
+        functionsIMP.mostvulnerable()
+      }
 
-    functionsIMP.leastvulnerable()
+      if (running=="9") {
+        functionsIMP.leastvulnerable()
+      }
 
-    functionsIMP.costumvulnerable()
+      if (running=="10") {
+        functionsIMP.costumvulnerable()
+      }
 
-    //functionsIMP.generalquery()
-
+      //functionsIMP.generalquery()
+      if (running == "End"){
+        println("Status----------------->Closing Application")
+      }
+    }
     functionsIMP.spark.close()
   }
 }
